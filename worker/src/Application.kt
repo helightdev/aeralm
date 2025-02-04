b4e86c7809d6c64d58589b24d9b8c96c40433e18
HelightDev <contact@helight.dev>
2025-02-04 12:42:30 +0100

    testing: switch to playwright because I can't get this to work with selenium

--- worker/src/Application.kt
+++ worker/src/Application.kt
@@ -1,10 +1,12 @@
 package dev.helight.aeralm
 
+import com.microsoft.playwright.Playwright
+import com.microsoft.playwright.impl.driver.Driver
 import io.ktor.server.application.*
 import org.ktorm.database.Database
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.manager.SeleniumManager
+import java.io.File
+import java.io.IOException
+import java.util.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -14,18 +16,43 @@ fun Application.module() {
     val db = Database.connect(
         "jdbc:sqlite:aeralm.db",
     )
-    val driver = ChromeDriver(ChromeOptions().apply {
-        addArguments("--headless")
-    })
-    driver.get("https://html.duckduckgo.com/html?q=test&s=10&kl=de-de")
-    val content = driver.executeScript("return document.documentElement.outerHTML") as String
-    println(content)
-    driver.quit()
-    println("Driver quit")
+
+    val playwrightEnv = mapOf(
+        "PLAYWRIGHT_BROWSERS_PATH" to File("browsers").absolutePath,
+        "PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD" to "1"
+    )
+
+    println("Installing playwright")
+    playwrightMain(arrayOf("install", "chromium"), playwrightEnv)
+    println("Installed playwright")
+    Playwright.create(Playwright.CreateOptions().setEnv(playwrightEnv)).use {
+        val chrome = it
+            .chromium()
+            .launch()
+        val page = chrome.newPage()
+        page.waitForLoadState()
+        page.navigate("https://html.duckduckgo.com/html?q=Schwarzwald&s=10&kl=de-de")
+        println(page.content())
+    }
+
 
     configureSecurity()
     configureHTTP()
     configureSerialization()
     configureSockets()
     configureRouting()
+}
+
+private fun playwrightMain(args: Array<String>, env: Map<String, String>) {
+    val driver = Driver.ensureDriverInstalled(emptyMap(), false)
+    val pb = driver.createProcessBuilder()
+    pb.command().addAll(listOf(*args))
+    val version = Playwright::class.java.getPackage().implementationVersion
+    if (version != null) {
+        pb.environment()["PW_CLI_DISPLAY_VERSION"] = version
+    }
+    pb.environment().putAll(env)
+    pb.inheritIO()
+    val process = pb.start()
+    process.waitFor()
 }
